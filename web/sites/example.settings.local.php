931afbb487f16d0b3e0a6736f302a61adb51ee73
Florian Weber <florian.weber@ueberbit.de>
2015-08-11 00:42:24 +0200

    Update core with 8.0.0-beta14

--- web/sites/example.settings.local.php
+++ web/sites/example.settings.local.php
@@ -12,12 +12,33 @@
  */
 
 /**
+ * Assertions.
+ *
+ * The Drupal project primarily uses runtime assertions to enforce the
+ * expectations of the API by failing when incorrect calls are made by code
+ * under development.
+ *
+ * @see http://php.net/assert
+ * @see https://www.drupal.org/node/2492225
+ *
+ * If you are using PHP 7.0 it is strongly recommended that you set
+ * zend.assertions=1 in the PHP.ini file (It cannot be changed from .htaccess
+ * or runtime) on development machines and to 0 in production.
+ *
+ * @see https://wiki.php.net/rfc/expectations
+ */
+assert_options(ASSERT_ACTIVE, 1);
+
+/**
  * Enable local development services.
  */
 $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
 
 /**
  * Show all error messages, with backtrace information.
+ *
+ * In case the error level could not be fetched from the database, as for
+ * example the database connection failed, we rely only on this value.
  */
 $config['system.logging']['error_level'] = 'verbose';
 
