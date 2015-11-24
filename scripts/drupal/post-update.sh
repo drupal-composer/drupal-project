1f018433381b63284a9ab0c0bc387905569452f3
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-11-24 12:04:27 -0800

    Ensure that scaffolding files are correctly updated, and updated on every 'composer update', when a dev version is being used.

--- scripts/drupal/post-update.sh
+++ scripts/drupal/post-update.sh
@@ -9,8 +9,12 @@ if [ -f $VERSION_FILE ]
 then
 
   # Get the Drupal version after `composer update`, and compare it with
-  # the version we recorded prior to the update running
-  DRUPAL_VERSION=$($DRUSH --root=$DRUPAL_ROOT status "Drupal version" --format=yaml | awk '{print $2}')
+  # the version we recorded prior to the update running.  Note that we
+  # convert the versions similar to '8.0.0-dev' to '8.0.x' here. This
+  # ensures that the version string is parsable by `drush dl`, and also
+  # that the scaffolding will be updated on every run of `composer update`
+  # for dev versions, because we do not do this transformation for $PREVIOUS_VERSION.
+  DRUPAL_VERSION=$($DRUSH --root=$DRUPAL_ROOT status "Drupal version" --format=yaml | awk '{print $2}' | sed -e 's/0-dev$/x/')
   PREVIOUS_VERSION=$(cat $VERSION_FILE)
   rm $VERSION_FILE
 
