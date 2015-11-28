59fc82563d745fbb7d66e839e96a308ffd05714b
Sven <sven@5sw.de>
2015-11-28 14:10:39 +0100

    settings.php and services.yml need not be executable.

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -4,14 +4,14 @@
 if [ ! -f web/sites/default/settings.php ]
   then
     cp web/sites/default/default.settings.php web/sites/default/settings.php
-    chmod 777 web/sites/default/settings.php
+    chmod 666 web/sites/default/settings.php
 fi
 
 # Prepare the services file for installation
 if [ ! -f web/sites/default/services.yml ]
   then
     cp web/sites/default/default.services.yml web/sites/default/services.yml
-    chmod 777 web/sites/default/services.yml
+    chmod 666 web/sites/default/services.yml
 fi
 
 # Prepare the files directory for installation
