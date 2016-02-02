a34a5d9d2b832cfc329029a7a56dfffa8160a8dd
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-02-01 16:18:56 -0800

    Put in the rest of the post-install script.

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
-# Check to see if we need to run drupal-scaffold
-if [ ! -f web/autoload.php ]
+DOCUMENTROOT=web
+
+# Prepare the scaffold files if they are not already present
+if [ ! -f $DOCUMENTROOT/autoload.php ]
   then
     composer drupal-scaffold
+    mkdir -p $DOCUMENTROOT/modules
+    mkdir -p $DOCUMENTROOT/themes
+    mkdir -p $DOCUMENTROOT/profiles
 fi
 
-
 # Prepare the settings file for installation
-if [ ! -f web/sites/default/settings.php ]
+if [ ! -f $DOCUMENTROOT/sites/default/settings.php ]
   then
-    cp web/sites/default/default.settings.php web/sites/default/settings.php
-    chmod 666 web/sites/default/settings.php
+    cp $DOCUMENTROOT/sites/default/default.settings.php $DOCUMENTROOT/sites/default/settings.php
+    chmod 666 $DOCUMENTROOT/sites/default/settings.php
     echo "Create a sites/default/settings.php file with chmod 666"
 fi
 
 # Prepare the services file for installation
-if [ ! -f web/sites/default/services.yml ]
+if [ ! -f $DOCUMENTROOT/sites/default/services.yml ]
   then
-    cp web/sites/default/default.services.yml web/sites/default/services.yml
-    chmod 666 web/sites/default/services.yml
+    cp $DOCUMENTROOT/sites/default/default.services.yml $DOCUMENTROOT/sites/default/services.yml
+    chmod 666 $DOCUMENTROOT/sites/default/services.yml
     echo "Create a sites/default/services.yml services.yml file with chmod 666"
 fi
 
 # Prepare the files directory for installation
-if [ ! -d web/sites/default/files ]
+if [ ! -d $DOCUMENTROOT/sites/default/files ]
   then
     mkdir web/sites/default/files
     chmod 777 web/sites/default/files
