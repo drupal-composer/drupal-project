cd416c0f71edc8cf307219ff282dfe4cac80f06e
fago <fago@wolfgangziegler.net>
2016-03-03 17:24:51 +0100

    Add script for processing replacements.

---
+++ scripts/composer/process-replacements.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+
+// We get the project name from the name of the path that Composer created for
+// us.
+$project = basename(realpath("."));
+echo "Project name $project taken from directory name\n";
+
+// Specify files for which replacement will be applied.
+$file_patterns = [
+  'devsetup/*\.yml',
+  'web/sites/default/*\.php',
+];
+
+$replacements = [
+  "{{ project }}" => $project
+];
+
+// Process replacements.
+foreach ($file_patterns as $pattern) {
+
+  foreach (glob($pattern, GLOB_BRACE) as $file) {
+    $content = file_get_contents($file);
+    if (($new_content = strtr($content, $replacements)) != $content) {
+      echo "Processing replacements in file $file...\n";
+      file_put_contents($file, $new_content);
+    }
+  }
+}
+
+exit(0);
