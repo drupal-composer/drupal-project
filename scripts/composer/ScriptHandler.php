75e8700085cf94f9991f61be8520518a5576d69f
Pieter Frenssen <pieter@frenssen.be>
2016-06-25 14:08:53 +0200

    Fix permissions of directories and files in the files/ folder.

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -37,23 +37,23 @@ class ScriptHandler {
     // Prepare the settings file for installation
     if (!$fs->exists($root . '/sites/default/settings.php') and $fs->exists($root . '/sites/default/default.settings.php')) {
       $fs->copy($root . '/sites/default/default.settings.php', $root . '/sites/default/settings.php');
-      $fs->chmod($root . '/sites/default/settings.php', 0666);
-      $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
+      $fs->chmod($root . '/sites/default/settings.php', 0640);
+      $event->getIO()->write("Create a sites/default/settings.php file with chmod 0640");
     }
 
     // Prepare the services file for installation
     if (!$fs->exists($root . '/sites/default/services.yml') and $fs->exists($root . '/sites/default/default.services.yml')) {
       $fs->copy($root . '/sites/default/default.services.yml', $root . '/sites/default/services.yml');
-      $fs->chmod($root . '/sites/default/services.yml', 0666);
-      $event->getIO()->write("Create a sites/default/services.yml file with chmod 0666");
+      $fs->chmod($root . '/sites/default/services.yml', 0640);
+      $event->getIO()->write("Create a sites/default/services.yml file with chmod 0640");
     }
 
-    // Create the files directory with chmod 0777
+    // Create the files directory with chmod 0755
     if (!$fs->exists($root . '/sites/default/files')) {
       $oldmask = umask(0);
-      $fs->mkdir($root . '/sites/default/files', 0777);
+      $fs->mkdir($root . '/sites/default/files', 0755);
       umask($oldmask);
-      $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
+      $event->getIO()->write("Create a sites/default/files directory with chmod 0755");
     }
   }
 
