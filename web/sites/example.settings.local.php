f1bcb6c8744b2a44eb2ee6ac5d663f2de35c08c8
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2016-08-25 16:28:04 +0200

    Customize example.settings.local.php.

---
+++ web/sites/example.settings.local.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Local development overrides. This overrides are ignored by Git.
+ */
+
+// Note: To activate this file, rename it to "settings.local.php".
+
+# $settings['cache']['bins']['render'] = 'cache.backend.null';
+# $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+# $settings['extension_discovery_scan_tests'] = FALSE;
