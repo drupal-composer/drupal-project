1cca40074895d9a87233fd70ce422c8c2537ec12
Derek DeRaps <hawkeye@tenderwolf.io>
2017-02-13 14:57:56 +0000

    Add a production-only settings file
    This has settings in it for things like Trusted Host validation.

---
+++ web/sites/settings.production.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Production-specific configuration settings.
+ */
+
+$settings['trusted_host_patterns'] = array(
+  '^stage.redesign.fairtradeusa.org.771elmp01.blackmesh.com$',
+  '^prod.redesign.fairtradeusa.org.771elwb02.blackmesh.com$',
+  '^(www.)?fairtradeusa.org$',
+);
