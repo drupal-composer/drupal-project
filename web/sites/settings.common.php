0da120a494d10b18a66c7166ff4fb9ef7fba51aa
Derek DeRaps <hawkeye@tenderwolf.io>
2017-02-13 14:57:08 +0000

    Add 'file_scan_ignore_directories' to settings.php
    The 'file_scan_ignore_directories' setting has no default value, so we
    must set it manually.

--- web/sites/settings.common.php
+++ web/sites/settings.common.php
@@ -6,15 +6,21 @@
  */
 
 // Load services definition file.
-$settings['container_yamls'][] = __DIR__ . '/services.yml';
+$settings['container_yamls'][] = "$app_root/sites/services.yml";
+
+// Define the default list of folders that will be ignored by Drupal's file API.
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
 
 // Include settings specific to sites hosted on Pantheon.
-if (file_exists(__DIR__ . '/default/settings.pantheon.php')) {
+if (file_exists("$app_root/sites/default/settings.pantheon.php")) {
   // n.b. The settings.pantheon.php file makes some changes that affect all
   // envrionments that this site exists in. Always include this file, even in a
   // local development environment, to insure that the site settings remain
   // consistent.
-  include __DIR__ . '/default/settings.pantheon.php';
+  require "$app_root/sites/default/settings.pantheon.php";
 }
 
 // Include settings specific to sites hosted on Acquia.
