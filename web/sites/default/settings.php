0bf037ff25f3cc9bf7abcef86bee5376442b062e
Derek DeRaps <hawkeye@tenderwolf.io>
2017-02-13 14:54:30 +0000

    Replace "include settings" function with manual
    The "include settings" function didn't work for some reason, so we just
    fall back on the easier-to-read-anyhow manual includes.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -5,4 +5,14 @@
  * Includes the settings files appropriate for development environments.
  */
 
-_include_settings(array('development'));
+// Always include the "common" settings first.
+require "$app_root/sites/settings.common.php";
+
+// Include environmental settings next.
+require "$app_root/sites/settings.development.php";
+
+// Load local environment override configuration, if available.
+// Keep this code block at the end of this file to take full effect.
+if (file_exists("$app_root/$site_path/settings.local.php")) {
+  include "$app_root/$site_path/settings.local.php";
+}
--- web/sites/sites.php
+++ web/sites/sites.php
@@ -9,26 +9,3 @@
 $sites['example.com']
   = $sites['stage.example.com']
     = 'production';
-
-/**
- * Includes the settings files appropriate to the environment.
- *
- * @param array $environments
- *   A list of environments with matching settings files to include.
- */
-function _include_settings(array $environments = array()) {
-
-  // Always include the "common" settings first.
-  include DRUPAL_ROOT . '/sites/settings.common.php';
-
-  // Include environmental settings next.
-  foreach ($environments as $environment) {
-    include DRUPAL_ROOT . "/sites/settings.$environment.php";
-  }
-
-  // Always include local environment settings last, if the file exists.
-  $local_settings = DRUPAL_ROOT . '/sites/default/settings.local.php';
-  if (file_exists($local_settings)) {
-    include $local_settings;
-  }
-}
