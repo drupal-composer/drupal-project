b12d492fabe54b16ce7cd25d3771b15858c57aed
Derek DeRaps <hawkeye@tenderwolf.io>
2017-02-13 16:12:45 +0000

    Replace client settings with generic ones

--- web/sites/settings.production.php
+++ web/sites/settings.production.php
@@ -6,7 +6,5 @@
  */
 
 $settings['trusted_host_patterns'] = array(
-  '^stage.redesign.fairtradeusa.org.771elmp01.blackmesh.com$',
-  '^prod.redesign.fairtradeusa.org.771elwb02.blackmesh.com$',
-  '^(www.)?fairtradeusa.org$',
+  '^www\.example\.com$',
 );
