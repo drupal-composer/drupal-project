00a590094c3b30ae54a9795914cd48c27ae52f29
Daniel Toader <daniel.toader@thinkshout.com>
2017-04-11 12:13:00 -0700

    Update composer and config.

--- RoboFile.php
+++ RoboFile.php
@@ -8,6 +8,18 @@
 
 class RoboFile extends \ThinkShout\RoboDrupal\Tasks
 {
-
-
+  /**
+   * Cleanup migrations.
+   *
+   * @option string migrations
+   *   Optional list of migrations to reset, separated by commmas.
+   */
+  public function migrateCleanup($opts = ['migrations' => '']) {
+    $migrations = explode(',', $opts['migrations']);
+    $project_properties = $this->getProjectProperties();
+    foreach ($migrations as $migration) {
+      $this->taskExec('drush mrs ' . $migration)->dir($project_properties['web_root'])->run();
+    }
+    $this->taskExec('drush mr --all && drush pmu tt_migrate_google_sheets -y && drush en tt_migrate_google_sheets -y && drush ms')->dir($project_properties['web_root'])->run();
+  }
 }
