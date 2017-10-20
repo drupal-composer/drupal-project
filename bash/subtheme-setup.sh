dda82816fb23177eeb942fdb931c321aee624238
mdyoung3 <mdyoung3@asu.edu>
2017-10-20 20:13:00 +0000

    Fixed bash script to properly rename files and edit files.

--- bash/subtheme-setup.sh
+++ bash/subtheme-setup.sh
@@ -3,8 +3,7 @@
 RTD_DIR=web/themes/rtdoked
 
 cp -R web/themes/contrib/bootstrap/starterkits/cdn  $RTD_DIR
-#cd $RTD_DIR
-#rename starterkit info THEMENAME.starterkit.yml 
-#find . -type f -name 'THEMENAME.*' -exec rename THEMENAME rtdoked '{}' \;
-#find . -type f -name '*.yml' -exec sed -i 's/THEMETITLE/ASU Theme/' {} \;
-#find . -name "*.yml" -exec sed -i  s/THEMENAME/rtdoked/g {} \;
+rename starterkit info $RTD_DIR/THEMENAME.starterkit.yml
+find $RTD_DIR -type f -name 'THEMENAME.*' -exec rename THEMENAME rtdoked '{}' \;
+find $RTD_DIR -type f -name '*.yml' -exec sed -i 's/THEMETITLE/ASU Theme/' {} \;
+find $RTD_DIR -type f -name "*.yml" -exec sed -i  s/THEMENAME/rtdoked/g {} \;
