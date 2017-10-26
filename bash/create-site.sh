e3fd6c853d91a8022701dddbf7d1ea31959936e0
Maximo Mena <maximo.mena@gmail.com>
2017-10-26 01:59:11 -0700

    Remove broken 'drush si' command from create-site script. A vhost entry for ssl.

--- bash/create-site.sh
+++ bash/create-site.sh
@@ -3,5 +3,9 @@
 PROJECT_NAME=$(basename "$(pwd)")
 WEBSERVER=$PROJECT_NAME"_webserver_1"
 docker exec -i $WEBSERVER bash -c "cd /var/www/app \
- && composer create-project \
- && ./vendor/bin/drush site-install --db-url=mysql://root:password@localhost:3306/drupal"
+ && composer create-project"
+
+# Attempt to run drush si currently not working because
+# drush tries to call mysql, which doesn't exist on the server container
+# docker exec -i $WEBSERVER bash -c "cd /var/www/app/web \
+# && ../vendor/bin/drush site-install --db-url=mysql://root:password@localhost:3306/drupal"
