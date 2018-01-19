b80b8e015d3c1d1a4cd18ece1000c1d7777ace33
fago <nuppla@zites.net>
2018-01-19 10:43:25 +0100

    Add headless chrome support when running behat.

---
+++ scripts/run-tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+ARGS=""
+
+if [[ $HEADLESS = "1" ]]; then
+  ARGS="--disable-gpu --headless"
+fi
+
+(google-chrome-stable $ARGS --remote-debugging-address=127.0.0.1 --remote-debugging-port=9222 )&
+
+cd `dirname $0`/..
+./vendor/bin/behat -p vagrant --colors $@
+
+# End with stopping all sub-process; i.e. chrome.
+[[ -z "$(jobs -p)" ]] || kill $(jobs -p)
