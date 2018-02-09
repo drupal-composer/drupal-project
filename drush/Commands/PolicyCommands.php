a4b5a80bca2a82eec7d425d6008f1408acb1fb5c
Moshe Weitzman <weitzman@tejasa.com>
2018-02-09 15:50:56 -0500

    Update /drush dir for Drush9
    Shows how the benefits of a policyfile and also illustrates where to store site aliases and druh config.

---
+++ drush/Commands/PolicyCommands.php
@@ -0,0 +1,55 @@
+<?php
+namespace Drush\Commands;
+
+use Consolidation\AnnotatedCommand\CommandData;
+use Drush\Commands\DrushCommands;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Edit this file to reflect your organization's needs.
+ */
+
+class PolicyCommands extends DrushCommands {
+
+  /**
+   * Prevent catastrophic braino. Note that this file has to be local to the
+   * machine that initiates the sql:sync command.
+   *
+   * hook validate sql:sync
+   *
+   * @throws \Exception
+   */
+  public function sqlSyncValidate(CommandData $commandData) {
+    if ($commandData->input()->getArgument('destination') == '@prod') {
+      throw new \Exception(dt('Per !file, you may never overwrite the production database.', ['!file' => __FILE__]));
+    }
+  }
+
+  /**
+   * Limit rsync operations to production site.
+   *
+   * hook validate core:rsync
+   */
+  public function rsyncValidate(CommandData $commandData) {
+    if (preg_match("/^@prod/", $commandData->input()->getArgument('destination'))) {
+      throw new \Exception(dt('Per !file, you may never rsync to the production site.', ['!file' => __FILE__]));
+    }
+  }
+
+  /**
+   * Unauthorized may not execute updates.
+   *
+   * @hook validate updatedb
+   */
+  public function validateUpdateDb(CommandData $commandData) {
+    if (!$commandData->input()->getOption('secret') == 'mysecret') {
+      throw new \Exception(dt('UpdateDb command requires a secret token per site policy.'));
+    }
+  }
+
+  /**
+   * @hook option updatedb
+   * @option secret A required token else user may not run updatedb command.
+   */
+  public function optionsetUpdateDb($options = ['secret' => self::REQ]) {}
+}
