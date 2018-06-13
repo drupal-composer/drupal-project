6ab2ccc56aac8f3edaa7a8cbb773febec5841491
fago <fago@wolfgangziegler.net>
2018-06-13 18:57:21 +0200

    Correct docker variable value replacment.

--- scripts/composer/process-replacements.php
+++ scripts/composer/process-replacements.php
@@ -14,7 +14,6 @@ echo "Project name $project taken from directory name\n";
 // Specify files for which replacement will be applied.
 $file_patterns = [
   '*.md',
-  '*.env',
   'dotenv/*.env',
   'phapp.yml',
   'tests/behat/behat.yml',
