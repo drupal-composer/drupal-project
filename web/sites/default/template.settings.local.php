3c0704181bb8d8fa7b945e569b5a03f3a4d68f5f
Jelle den Butter <j.den.butter@frmwrk.nl>
2019-10-15 09:21:19 +0200

    [DD-000] Update composer libraries and add frmwrk_decoupled
    Added files and private files to ignore

---
+++ web/sites/default/template.settings.local.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Include the default default settings
+ */
+if (file_exists($app_root . '/' . $site_path . '/settings.frmwrk.php')) {
+  include $app_root . '/' . $site_path . '/settings.frmwrk.php';
+}
