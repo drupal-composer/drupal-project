561f0d2486a1787b54186c8fc0c9a4c929ec1921
Florian Weber <florian.weber@ueberbit.de>
2019-12-05 23:26:22 +0100

    Update dependencies

--- load.environment.php
+++ load.environment.php
@@ -11,10 +11,5 @@ use Dotenv\Exception\InvalidPathException;
 /**
  * Load any .env file. See /.env.example.
  */
-$dotenv = new Dotenv(__DIR__);
-try {
-  $dotenv->load();
-}
-catch (InvalidPathException $e) {
-  // Do nothing. Production environments rarely use .env files.
-}
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
