0c8741d97ec9b2c52d82b1a8a2d1ed1c6a190203
mikemccaffrey <github@mikemccaffrey.net>
2020-05-17 16:56:34 -0700

    Add warning that settings.php will be overwritten. (kalamuna/drupal-project#85)

--- assets/append.settings.php
+++ assets/append.settings.php
@@ -1,4 +1,12 @@
 /**
- * Append the config directory settings to the default settings.php provided by the pantheon scaffold.
+ * Appended by drupal-project to settings.php provided by the pantheon scaffold.
  */
+
+// Specify the location of our config sync directory.
 $settings['config_sync_directory'] = '../config/sync';
+
+/**
+ * WARNING: ADDITIONS OR CHANGES TO THIS FILE WILL BE OVERWRITTEN BY COMPOSER.
+ *
+ * Before making custom changes, remove "[web-root]/sites/default/settings.php" from the "file-mapping" array in composer.json.
+ */
