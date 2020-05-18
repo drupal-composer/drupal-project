4e9d339a5a95f46c9c1ff3674bcd697998f5f531
mikemccaffrey <github@mikemccaffrey.net>
2020-05-17 17:07:50 -0700

    Don't send email on non-live environments. (kalamuna/drupal-project#80)

--- assets/append.settings.php
+++ assets/append.settings.php
@@ -5,6 +5,13 @@
 // Specify the location of our config sync directory.
 $settings['config_sync_directory'] = '../config/sync';
 
+// Make sure that only the live environment can send out emails.
+if (!isset($_ENV['PANTHEON_ENVIRONMENT']) || $_ENV['PANTHEON_ENVIRONMENT'] !== 'live') {
+  $conf['mail_system'] = array(
+    'default-system' => 'DevelMailLog',
+  );
+}
+
 /**
  * WARNING: ADDITIONS OR CHANGES TO THIS FILE WILL BE OVERWRITTEN BY COMPOSER.
  *
